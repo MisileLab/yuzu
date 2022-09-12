4e704d9a72bdf30fdc6a896a0c0cc3f16e7bfe08
misilelaboratory <misileminecord@gmail.com>
2022-09-12 20:26:11 +0900

    some init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use bollard::{Docker, errors::Error::DockerResponseServerError};
+use futures_util::future::FutureExt;
+use std::{
+    time::Duration,
+    thread::sleep,
+    process::Command
+};
+
 fn main() {
-  println!("hello, miku!")
+  match Docker::connect_with_local_defaults() {
+      Ok(_) => {},
+      Err(a) => {
+          match a {
+              DockerResponseServerError{..} => {
+                    println!("Docker does not response, Install docker please"); 
+              },
+              _ => {}
+          }
+      }
+  }
 }
