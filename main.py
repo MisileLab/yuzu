99e79c32792429040988cb4a24c740838e445103
misilelaboratory <misileminecord@gmail.com>
2022-10-09 01:27:30 +0900

    some update

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import docker
 import click
 from docker.models.containers import Container
 from docker.models.images import Model
-from docker.errors import APIError, NotFound
+from docker.errors import NotFound
 from misilelibpy import check_path
-from os.path import isfile
-from os import remove
+from os.path import isdir, isfile
+from os import mkdir, remove
 from tomli import loads
 
 class Dockers:
@@ -13,27 +13,32 @@ class Dockers:
         self.client = docker.from_env()
 
     def setup_some(self):
+        if isdir("yuzu") is False:
+            mkdir("yuzu")
         if isfile("yuzu.toml"):
             config = loads(open("yuzu.toml", "r").read())
         else:
-            container: Model = self.client.containers.run("eclipse-temurin:8-jre-jammy", detach=True) # type: ignore
+            container: Model = self.client.containers.run("eclipse-temurin:8-jre-jammy", detach=True, mounts=[docker.types.Mount("yuzu", "yuzu", type="bind")]) # type: ignore
             config = {"id": container.id_attribute}
         try:
             c: Container = self.client.containers.get(config["id"]) # type: ignore
-        except (APIError, NotFound):
+        except NotFound:
             remove("yuzu.toml")
             self.setup_some()
             return
         else:
             if c.status != "running":
                 c.start()
-            c.exec_run("sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update && sudo apt dist-upgrade && sudo apt install python3.10")
-            c.exec_run("curl -sSL example.sh | python -") # i'll change it
+            c.exec_run("sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt install python3.10")
+            c.exec_run("curl -sSL https://install.python-poetry.org | python3 -")
+            c.exec_run("git clone https://github.com/misilelab/midori yuzu/midori && cd yuzu/midori && poetry install && cd -")
 
     def update_some(self):
         config = loads(open("yuzu.toml", "r").read())["id"]
+        self.client.images.pull('eclipse-temurin:8-jre-jammy')
+        self.client.images.pull('eclipse-temurin:17-jre-jammy')
         c = self.client.containers.get(config)
-        c.exec_run("sudo apt update && sudo apt dist-upgrade") # type: ignore
+        c.exec_run("sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y") # type: ignore
 
     def link_some(self):
         raise NotImplementedError
