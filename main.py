bf078c1adc45b1fe9521b37076d6a6af9de77d3a
misilelaboratory <misileminecord@gmail.com>
2022-10-09 23:20:50 +0900

    let's go to link system

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import click
 from docker.models.containers import Container
 from docker.models.images import Model
 from docker.errors import NotFound
-from misilelibpy import check_path
+from misilelibpy import check_path, read_once, write_once
 from os.path import isdir, isfile
 from os import mkdir, remove
 from tomli import loads
+from json import loads, dumps
 
 class Dockers:
     def __init__(self):
@@ -31,17 +32,29 @@ class Dockers:
                 c.start()
             c.exec_run("sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt install python3.10")
             c.exec_run("curl -sSL https://install.python-poetry.org | python3 -")
-            c.exec_run("git clone https://github.com/misilelab/midori yuzu/midori && cd yuzu/midori && poetry install && cd -")
+            if isdir(check_path("yuzu/midori")) == False:
+                c.exec_run("git clone https://github.com/misilelab/midori yuzu/midori && cd yuzu/midori && poetry install && cd -")
+            else:
+                c.exec_run("cd yuzu/midori && git pull && poetry install && cd -")
 
     def update_some(self):
         config = loads(open("yuzu.toml", "r").read())["id"]
         self.client.images.pull('eclipse-temurin:8-jre-jammy')
         self.client.images.pull('eclipse-temurin:17-jre-jammy')
-        c = self.client.containers.get(config)
-        c.exec_run("sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y") # type: ignore
+        c: Container = self.client.containers.get(config) # type: ignore
+        c.exec_run("sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y")
+        c.exec_run("cd yuzu/midori && git pull && poetry install && cd -")
 
-    def link_some(self):
-        raise NotImplementedError
+    def link_some(self, version: str):
+        if isfile(check_path("yuzu/yuzu.json")) == False:
+            write_once(check_path('yuzu/yuzu.json'), '{}')
+        config: dict = loads(read_once(check_path('yuzu/yuzu.json')))
+        if config.get(version) is None:
+            # TODO: install and add to config
+            write_once(check_path('yuzu/yuzu.json'), dumps(config))
+        else:
+            # TODO: use libraries in yuzu/yuzu.json
+            pass
 
     def run_java(self):
         raise NotImplementedError
@@ -51,11 +64,11 @@ class Dockers:
 @click.option('--ram', type=int, default=8, help="minecraft ram, default is 8GB")
 @click.option('--dir', type=str, help="dir of server")
 @click.command("create")
-def main():
+def main(version: str, mversion: str, ram: int, dir: str):
     dockers = Dockers()
     dockers.setup_some()
     dockers.update_some()
-    dockers.link_some()
+    dockers.link_some(version)
     dockers.run_java()
 
 @click.command("list")
